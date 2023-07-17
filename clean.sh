6cabc6a266498d214241f4d7e0dc3f2881c505eb
Bguinet <39563212+BenjaminGuinet@users.noreply.github.com>
2023-07-17 21:53:55 +0200

    Add files via upload

---
+++ clean.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+rm -rf main.a*
+rm -rf main.b*
+rm -rf main.g*
+rm -rf main.i*
+rm -rf main.l*
+rm -rf main.m*
+rm -rf main.pdf
+rm -rf main.out
+rm -rf main.synctex.gz
+rm -rf main.toc
+rm -rf *.aux
+rm -rf */*.aux
+rm -rf */*.bbl
+rm -rf */*.log
